1703dc9f8e9e6ee40c681f89336a0ebb1b475776
suptan <s.tansutirapong@gmail.com>
2020-03-31 09:20:05 +0700

    test(store): add more test to product store

--- book-shop/test/unit/specs/store/modules/products/actions.spec.js
+++ book-shop/test/unit/specs/store/modules/products/actions.spec.js
@@ -8,10 +8,15 @@ jest.mock('@/utils/logger', () => ({
 }));
 
 describe('products/actions', () => {
-  describe('getBooks', () => {
+  let mockCommit;
+
+  beforeEach(() => {
+    mockCommit = jest.fn();
+  });
+
+  describe('getBooks()', () => {
     it('should commit correct mutation', async () => {
       const mockData = 1;
-      const mockCommit = jest.fn();
       api.fetchBooks = jest.fn(() => Promise.resolve(mockData));
       await actions.getBooks({ commit: mockCommit });
 
@@ -27,11 +32,19 @@ describe('products/actions', () => {
 
     it('should log error when can\'t fetch data', async () => {
       const mockError = { msg: 'some error' };
-      const mockCommit = jest.fn();
       api.fetchBooks = jest.fn(() => Promise.reject(mockError));
       await actions.getBooks({ commit: mockCommit });
 
       expect(logger.error).toHaveBeenCalledWith(JSON.stringify(mockError));
+    });
+  });
+
+  describe('filterBooks()', () => {
+    it('should commit to FILTER_BOOKS mutation', () => {
+      const input = 'foo';
+      actions.filterBooks({ commit: mockCommit }, input);
+
+      expect(mockCommit).toHaveBeenCalledWith('FILTER_BOOKS', input);
     });
   });
 });
