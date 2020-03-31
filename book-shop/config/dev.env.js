8687f5bc0702d71a247cd2ffa484c018577235bc
suptan <s.tansutirapong@gmail.com>
2020-03-31 23:03:22 +0700

    refactor(config): remove unused

--- book-shop/config/dev.env.js
+++ book-shop/config/dev.env.js
@@ -5,6 +5,5 @@ module.exports = merge(prodEnv, {
   ALLOW_LOG_LEVEL: '"debug"',
   NODE_ENV: '"development"',
   PROXY_URL: '"https://cors-anywhere.herokuapp.com/"',
-  ENABLED_PROXY: '"ON"',
   RESOURCE_URL: '"https://json-bin.netlify.com/books.json"',
 });
--- book-shop/config/prod.env.js
+++ book-shop/config/prod.env.js
@@ -1,6 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   PROXY_URL: '"https://cors-anywhere.herokuapp.com/"',
-  ENABLED_PROXY: '"ON"',
   RESOURCE_URL: '"https://json-bin.netlify.com/books.json"',
 };
